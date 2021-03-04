29953f0dc103144a55f22e83921028c41e61ec26
rizhan-ahmad <57541143+rizhan-ahmad@users.noreply.github.com>
2021-03-04 21:22:52 +0800

    201 Praktikum Android

---
+++ app/src/main/java/com/ahmadrizhan/praktikumandroid/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.ahmadrizhan.praktikumandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.ahmadrizhan.praktikumandroid.ui.user.UserViewModel
+import com.ahmadrizhan.praktikumandroid.R
+import com.ahmadrizhan.praktikumandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: UserViewModel by lazy {
+        ViewModelProvider(this).get(UserViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.progressBar.visibility = View.VISIBLE
+
+        viewModel.response.observe(this, {
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.tvMainActivity.text = it.data[1].last_name
+        })
+
+    }
+
+
+}
