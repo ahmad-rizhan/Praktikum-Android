c2d71c0f7f0934cf415ce19998fbc4d06ee7f358
rizhan-ahmad <57541143+rizhan-ahmad@users.noreply.github.com>
2021-03-13 19:59:04 +0800

    201 Praktikum Android

---
+++ app/src/main/java/com/ahmadrizhan/praktikumandroid/ui/jenisbarang/JenisbarangActivity.kt
@@ -0,0 +1,39 @@
+package com.ahmadrizhan.praktikumandroid.ui.jenisbarang
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ahmadrizhan.praktikumandroid.databinding.ActivityJenisbarangBinding
+import com.ahmadrizhan.praktikumandroid.model.JenisbarangData
+
+class JenisbarangActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityJenisbarangBinding
+    private val list = ArrayList<JenisbarangData>()
+    private val viewModel: JenisbarangViewModel by lazy {
+        ViewModelProvider(this).get(JenisbarangViewModel::class.java)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityJenisbarangBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvJenisbarang.setHasFixedSize(true)
+
+        binding.progressBarJenisbarang.visibility = View.VISIBLE
+
+        getListJenisbarang()
+
+    }
+
+    private fun getListJenisbarang() {
+        viewModel.response.observe(this, {
+            binding.progressBarJenisbarang.visibility = View.INVISIBLE
+            list.addAll(it.data)
+            binding.rvJenisbarang.layoutManager = LinearLayoutManager(this)
+            val listJenisbarangAdapter = ListJenisbarangAdapter(list)
+            binding.rvJenisbarang.adapter = listJenisbarangAdapter
+        })
+    }
+}
